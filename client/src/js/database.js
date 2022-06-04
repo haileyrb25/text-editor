3ecb8179b6918e00814f2789b8f2e600796c5460
Hailey Bates <hailey.bates001@gmail.com>
2022-06-04 01:37:20 -0400

    Added putDb on database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,8 +13,15 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+  const todosDb = await openDB('todos', 1);
+  const tx = todosDb.transaction('todos', 'readwrite');
+  const store = tx.objectStore('todos');
+  const request = store.put({ id: id, todo: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+};
 
 export const getDb = async () => {
   console.error('getDb not implemented');
